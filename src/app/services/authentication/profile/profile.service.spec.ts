14843c3d77b77ad654966cc4c67c3e57d8a8c253
Sangram Reddy <reddy.horcrux@gmail.com>
2019-03-21 00:26:10 +0000

    added authservice tests

--- src/app/services/authentication/profile/profile.service.spec.ts
+++ src/app/services/authentication/profile/profile.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProfileService } from './profile.service';
+import { VirtueCognitoService } from '../virtue-cognito/virtue-cognito.service';
+import { environment as testEnvironment } from '../../../../environments/environment.test';
+import { CognitoUser } from 'amazon-cognito-identity-js';
 
 describe('ProfileService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +11,15 @@ describe('ProfileService', () => {
   it('should be created', () => {
     const service: ProfileService = TestBed.get(ProfileService);
     expect(service).toBeTruthy();
+  });
+
+  it('Test initializeProfileUsingUserData', () => {
+    const service: ProfileService = TestBed.get(ProfileService);
+    const virtueCognitoService: VirtueCognitoService = TestBed.get(VirtueCognitoService)
+    service.initializeProfileUsingUserData({
+      Username: testEnvironment.phone_number,
+      Pool: virtueCognitoService.cognitoUserPool
+    })
+    expect(service.cognitoUser).toEqual(jasmine.any(CognitoUser))
   });
 });
