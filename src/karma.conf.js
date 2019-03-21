c029e4b2c7e9e38abbb6729a4984707001d27b8f
Sangram Reddy <reddy.horcrux@gmail.com>
2019-03-21 22:00:08 +0000

    Added codecoverage publish step

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'junit'],
+    reporters: ['progress', 'junit', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
