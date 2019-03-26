9770f97d6bd5b5a6eb562f37a23f88df04d29d22
Anirup Patnaik <anirup049@gmail.com>
2019-03-26 15:27:17 +0530

    Phone number validation changes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -39,7 +39,7 @@ export class RegisterComponent implements OnInit {
      this.confirmationCode = new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.minLength(6)])
 
     this.registerFormGroup = new FormGroup({
-      mobileNumber: new FormControl('', [Validators.required, Validators.min(100000000), Validators.max(9999999999)]),
+      mobileNumber: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.min(1000000000), Validators.max(9999999999)]),
       name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
