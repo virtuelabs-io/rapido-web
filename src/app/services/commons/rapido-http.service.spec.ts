9859f0895c7cce0d67ae8dc2a6ade645862a9b07
Sangram Reddy <reddy.horcrux@gmail.com>
2019-04-04 00:14:02 +0100

    fixed existing tests

--- src/app/services/commons/rapido-http.service.spec.ts
+++ src/app/services/commons/rapido-http.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RapidoHttpService } from './rapido-http.service';
 
 describe('RapidoHttpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: RapidoHttpService<string> = TestBed.get(RapidoHttpService);
--- src/app/services/products/products.service.spec.ts
+++ src/app/services/products/products.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: ProductsService = TestBed.get(ProductsService);
