036293736452abfc2888020d1919cad6ea221040
Anirup Patnaik <anirup049@gmail.com>
2019-04-10 03:08:16 +0530

    Account Info New UX

--- src/app/account-info/account-info.component.ts
+++ src/app/account-info/account-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../services/authentication/profile/profile.service';
+import { UpdateAttributeService } from '../services/authentication/update-attribute/update-attribute.service';
+import { Registration } from '../services/authentication/helpers/registration';
+import { DeleteUserService } from '../services/authentication/delete-user/delete-user.service';
 
 @Component({
   selector: 'app-account-info',
@@ -6,6 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-info.component.scss']
 })
 export class AccountInfoComponent implements OnInit {
+  _profileService: ProfileService;
+  panelOpenState = false
+  viewMode: Boolean = true
+  updateMode: Boolean = false
+
+  _updatedAttribute: Boolean = false;
+  private _deleteUserService: DeleteUserService
+
+  attribute = {
+    phone_number: null,
+    name: null,
+    email: null,
+    sendMePromotions: "false",
+    commViaEmail: "false"
+  }
 
   accountItems = [
     {
@@ -25,10 +44,57 @@ export class AccountInfoComponent implements OnInit {
     }
     
   ]
-
-  constructor() { }
+  private _updateAttributeService: UpdateAttributeService
+  constructor(
+    profileService: ProfileService,
+    updateAttributeService: UpdateAttributeService,
+    deleteUserService: DeleteUserService
+  ) {
+    this._profileService = profileService
+    this._updateAttributeService = updateAttributeService
+    this._deleteUserService = deleteUserService
+  }
 
   ngOnInit() {
+    let localAttributes = this.attribute
+    // this._profileService.cognitoUser.getUserAttributes(function(err, result){
+    //   if (err) {
+    //   //  reject(new Response( 1, err.message, err ))
+    //   }
+    //   console.log(result)
+    //   localAttributes.name = result[7].getValue()
+    //   localAttributes.phone_number = result[8].getValue()
+    //   localAttributes.email = result[10].getValue()
+    // })
+  }
+  edit() {
+    console.log(this._profileService)
+    this.viewMode = false
+    this.updateMode = true
+  }
+  update() {
+    this.viewMode = true
+    this.updateMode = false
+
+    console.log(this.attribute)
+    let registrationUpdate: Registration = new Registration(
+      this.attribute.phone_number,
+      this.attribute.email,
+      this.attribute.name,
+      "",
+      this.attribute.sendMePromotions,
+      this.attribute.commViaEmail
+
+    )
+    this._updateAttributeService.attributeList = registrationUpdate.createUpdateAttributeList()
+    const promise = this._updateAttributeService.updateAttributes()
+    promise.then(value => {
+      this._updatedAttribute = true;
+      console.log(value) // response from successfull resolve
+    }).catch(error => {
+      this._updatedAttribute = false;
+      console.log(error) // response from a graceful reject
+    })
   }
 
 }
