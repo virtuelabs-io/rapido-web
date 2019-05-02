3b8c24f322100b77ca1280eed161e2e7c3def3cd
Anirup Patnaik <anirup049@gmail.com>
2019-05-02 12:37:30 +0530

    Company Details component card

--- src/app/company-details/company-details.component.ts
+++ src/app/company-details/company-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyDetails } from '../services/customer/company-details';
+import { CompanyDetailsService } from '../services/customer/company-details.service';
 
 @Component({
   selector: 'app-company-details',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-details.component.scss']
 })
 export class CompanyDetailsComponent implements OnInit {
+  hide_plus_card: Boolean = true
+  companyItems = 
+  {
+    organisation: "",
+    add1: "",
+    add2: "",
+    town_city: "",
+    postcode: "",
+    country: "",
+    customer_id:""
 
-  constructor() { }
+  }
+  private _companyDetailsService: CompanyDetailsService
+  constructor(
+    companyDetailsService: CompanyDetailsService
+  ) { 
+    this._companyDetailsService = companyDetailsService
+  }
 
   ngOnInit() {
+    this._companyDetailsService.getCompanyDetails()
+    .subscribe(data => {
+      console.log("company details fetched !")
+      console.log(data)
+      this.companyItems.add1 = data.addr_1
+      this.companyItems.add2 = data.addr_2
+      this.companyItems.town_city = data.city
+      this.companyItems.organisation = data.company_name
+      this.companyItems.country = data.country
+      //this.companyItems.add1 = data.county
+      this.companyItems.customer_id = data.customer_id
+      this.companyItems.postcode = data.postcode
+      this.hide_plus_card = false
+    })
   }
 
 }
