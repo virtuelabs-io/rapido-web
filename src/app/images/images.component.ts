f3aece89c2380f772b94c8d4ecc25515261c5e2f
sharan <sharan09@in.ibm.com>
2019-06-22 18:26:58 +0530

    wip

---
+++ src/app/images/images.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-images',
+  templateUrl: './images.component.html',
+  styleUrls: ['./images.component.scss']
+})
+export class ImagesComponent implements OnInit {
+
+  @Input() imgSrc:string = "https://cdn.aws.toolstation.com/images/141020-UK/250/88614.jpg"
+  //160/17428.jpg
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
