c2fa1e1f871ffd8b413d97a7804cd4ec60cc9e9f
Anirup Patnaik <anirup049@gmail.com>
2019-06-26 12:47:18 +0530

    Code Refactoring of Address component

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -13,63 +13,48 @@ export class AddressComponent implements OnInit {
   address_details_result: string;
   private _addressDetailsService: AddressDetailsService
   address: any
-//   address = [
-  
-// ]
   example:any;
   constructor( 
     private router: Router,
     addressDetailsService: AddressDetailsService
-  ) { 
+  ) {
     this._addressDetailsService = addressDetailsService
-    const navigation = this.router.getCurrentNavigation();
-    // const state = navigation.extras.state as {example: string};
-    // if(state != undefined){
-    //   this.example = state.example;
-    // }
-
-    this._addressDetailsService.getAddressDetailsList()
-    .subscribe(data => {
-      console.log(data)
-      if(data['length'] > 0){
-        this.address_details_id = data[0]['id']
-        this.address = data
-        console.log("find address below")
-        console.log('Sucessfully updated the address test id to: ' + String(this.address_details_id))
-      }
-      this.address_details_result = "Sucessfully fetched address details List and logged!";
-    })
-   
+    this.getAddressList()
   }
 
   ngOnInit() {
     this.showSpinner = true
-    // setTimeout(() => {
-    //   this.showSpinner = false
-    // }, 5000)
     this._addressDetailsService.getAddressDetailsList()
     .subscribe(data => {
-      console.log(data)
       if(data['length'] > 0){
         this.address_details_id = data[0]['id']
         this.address = data
-        console.log("find address below")
-        console.log('Sucessfully updated the address test id to: ' + String(this.address_details_id))
       }
       this.address_details_result = "Sucessfully fetched address details List and logged!";
       this.showSpinner = false
     })
   }
+
+  getAddressList() {
+    this._addressDetailsService.getAddressDetailsList()
+    .subscribe(data => {
+      if(data['length'] > 0){
+        this.address_details_id = data[0]['id']
+        this.address = data
+      }
+      this.address_details_result = "Sucessfully fetched address details List and logged!";
+    })
+  }
+
   addressDelete(id) {
     this._addressDetailsService.deleteAddressDetails(id)
     .subscribe(data => {
-      console.log(data)
       this.address_details_id = null
       this.address_details_result = "Sucessfully deleted customer address details and logged!";
+      this.getAddressList()
     })
-    this.router.navigate(['profile']);
   }
   addressEdit(id) {
-    this.router.navigate(['profile/address/editAddress', id]);
+    this.router.navigate(['profile/address/editAddress', id])
   }
 }
