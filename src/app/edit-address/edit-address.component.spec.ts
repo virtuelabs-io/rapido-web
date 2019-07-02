cffd37220c2d212a2560f6ceaf9b782f234d91bd
Anirup Patnaik <anirup049@gmail.com>
2019-07-03 03:47:18 +0530

    Route test comment

--- src/app/edit-address/edit-address.component.spec.ts
+++ src/app/edit-address/edit-address.component.spec.ts
@@ -28,4 +28,23 @@ describe('EditAddressComponent', () => {
   it('should not create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*it('Mandatory fields during edit Address', async(() => {
+    component.id = 48
+    component.addressFormGroup.controls['name'].setValue("Sam")
+    component.addressFormGroup.controls['add1'].setValue("xyz")
+    component.addressFormGroup.controls['add2'].setValue("abc cdf")
+    component.addressFormGroup.controls['town_city'].setValue("Charles Land")
+    component.addressFormGroup.controls['postCode'].setValue("12345")
+    component.addressFormGroup.controls['county'].setValue("UK County")
+    component.addressFormGroup.controls['country'].setValue("UK")
+
+    expect(component.addressFormGroup.controls['name'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['add1'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['add2'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['town_city'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['postCode'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['county'].hasError('required')).toBeFalsy()
+    expect(component.addressFormGroup.controls['country'].hasError('required')).toBeFalsy()
+  }));*/
 });
