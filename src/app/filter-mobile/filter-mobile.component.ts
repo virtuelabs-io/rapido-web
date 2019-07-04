2755b85d816822bd80ce3b8996ffa2a21768fcfd
sharan <sharan09@in.ibm.com>
2019-07-04 17:43:19 +0530

    Adding filter controls in mobile mode

--- src/app/filter-mobile/filter-mobile.component.ts
+++ src/app/filter-mobile/filter-mobile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchItemService } from '../shared-services/search-item/search-item.services';
 
 @Component({
   selector: 'app-filter-mobile',
@@ -7,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterMobileComponent implements OnInit {
 
-  constructor() { }
-
+  
+  filterData: Object 
+  constructor(private _searchItemService: SearchItemService ) { }
+  fnPriceFilterHandler: Function;
+  
   ngOnInit() {
+    this.fnPriceFilterHandler= 
+              obj => this.priceFilterData(obj);
+    this.filterData = [
+                      {
+                        'headerText':'show result for',
+                         'panel':[ {
+                           'panelTitle':'watches',
+                           'panelType':'link',
+                           'panelData':['Sand','Tools']
+                        }]
+                      },
+                      {
+                        'headerText':'filter by',
+                         'panel':[ 
+                           {
+                          'panelTitle':'rating',
+                          'panelType':'rating',
+                          'panelData':["4","3","2","1"]
+                       },
+                        {
+                          'panelTitle':'price',
+                          'panelType':'priceslider',
+                          'panelData':{
+                            'fnPriceFilterHandler':this.fnPriceFilterHandler,
+                            'maxValue':500,
+                            'minValue':0
+                          }
+                       }]
+                      }
+         ]
   }
+  priceFilterData(range){
+    console.log(range.min,range.max)
+  }
+  onPressRating(value){
+    
+    this.changeQuery({q:'watches',sort:'desc'})
+  }
+  onPressItem(eve){
+    console.log(eve)
+  }
+  changeQuery(queryObj){
+      this._searchItemService.changeState(queryObj)
+  }
+
+
+
+
 
 }
