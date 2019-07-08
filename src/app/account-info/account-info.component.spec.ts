ef74b9863450c761c5cef77724605eca8ba74f8b
Anirup Patnaik <anirup049@gmail.com>
2019-07-08 14:43:45 +0530

    unable to fix the test case error
    Tried this approach but not working

--- src/app/account-info/account-info.component.spec.ts
+++ src/app/account-info/account-info.component.spec.ts
@@ -16,15 +16,37 @@ describe('AccountInfoComponent', () => {
       declarations: [ AccountInfoComponent ]
     })
     .compileComponents();
+    const someServiceStub = jasmine.createSpyObj('ProfileService', ['cognitoUser.getUserAttributes']);
+    someServiceStub.cognitoUser.getUserAttributes.and.returnValue(
+      [
+        {"CognitoUserAttribute": {Name: "sub", Value: "b91c285c-2b92-4401-9d1e-c883deb95a6b"}},
+        {"CognitoUserAttribute": {Name: "email_verified", Value: "false"}},
+        {"CognitoUserAttribute": {Name: "custom:acceptedTAndC", Value: "true"}},
+        {"CognitoUserAttribute": {Name: "custom:personalisation", Value: "false"}},
+        {"CognitoUserAttribute": {Name: "phone_number_verified", Value: "true"}},
+        {"CognitoUserAttribute": {Name: "custom:rapidoId", Value: "12e7c3cd-8606-43ec-a103-7b73709a42f1"}},
+        {"CognitoUserAttribute": {Name: "custom:commViaEmail", Value: "false"}},
+        {"CognitoUserAttribute": {Name: "name", Value: "Anirup Pat"}},
+        {"CognitoUserAttribute": {Name: "phone_number", Value: "+917032908112"}},
+        {"CognitoUserAttribute": {Name: "custom:sendMePromotions", Value: "false"}},
+        {"CognitoUserAttribute": {Name: "email", Value: "anirup049@gmail.com"}},
+        {"CognitoUserAttribute": {Name: "custom:commViaSMS", Value: "false"}}
+      ]
+    );
   }));
 
   beforeEach(() => {
+    
+
     fixture = TestBed.createComponent(AccountInfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    
   });
 
   it('should create', () => {
+    component.fetchUserProfile();
     expect(component).toBeTruthy();
   });
 });
