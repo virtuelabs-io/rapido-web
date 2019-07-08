00d8dfcf8cc4155ef45c77e5b2da59c12541e244
Anirup Patnaik <anirup049@gmail.com>
2019-07-08 18:39:33 +0530

    creating objects for cart items
    Cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
+  cartItems = [
+    {
+      icon: "#shopping-bag",
+      title: "Fossil Q Gen 4 Hr Gold Dial Men's Smart Watch-FTW4017",
+      amount: "21,995.00",
+      qunatity: "2"
+    },
+    {
+      icon: "#shopping-bag",
+      title: "Fossil Q Gen 4 Hr Gold Dial Men's Smart Watch-FTW4017",
+      amount: "21,995.00",
+      qunatity: "2"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
