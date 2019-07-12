228c8edc5448498b2d4b79938741d642c0e80542
Sangram Reddy <reddy.horcrux@gmail.com>
2019-07-12 21:22:29 +0100

    Refactored, tested and added service usage

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -13,4 +13,32 @@ export class CartService extends RapidoHttpService<CartItem> {
   constructor(protected _http: HttpClient, protected _profileService: ProfileService) {
     super(_http, _profileService)
   }
+
+  getCartItems(){
+    return this.getList([Constants.CART_APIS.api, 'items'].join("/"),this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  getInCartItems(){
+    return this.getList([Constants.CART_APIS.api, 'in-cart-items'].join("/"),this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  getSavedForLaterCartItems(){
+    return this.getList([Constants.CART_APIS.api, 'saved-for-later-items'].join("/"),this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  postCartItem(cartItem: CartItem){
+    return this.post([Constants.CART_APIS.api, 'item'].join("/"), cartItem, this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  postCartItemList(cartItems: CartItem[]){
+    return this.postList([Constants.CART_APIS.api, 'items'].join("/"), cartItems , this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  deleteCartItem(product_id: number){
+    return this.delete([Constants.CART_APIS.api, 'item',String(product_id)].join("/"),this.addAuthHeader(this.initializeHeaders()))
+  }
+
+  deleteInCartItem(){
+    return this.delete([Constants.CART_APIS.api, 'in-items'].join("/"),this.addAuthHeader(this.initializeHeaders()))
+  }
 }
