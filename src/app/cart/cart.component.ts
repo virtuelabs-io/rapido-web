e022c7440e75b53de6b493d58583468cbf4c9c78
Anirup Patnaik <anirup049@gmail.com>
2019-07-15 04:06:17 +0530

    Save for Later action
    cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Constants } from '../utils/constants';
+import { CartItem } from '../services/cart/cart-item';
 
 @Component({
   selector: 'app-cart',
@@ -9,28 +10,8 @@ import { Constants } from '../utils/constants';
 })
 export class CartComponent implements OnInit {
   _imageUrl: string = Constants.environment.staticAssets
-  cartItems = [
-    
-   /* {
-      icon: "https://s3.eu-west-2.amazonaws.com/rapido-build-assets-bucket/images/products/1/image1.jpg",
-      title: "ROMEX SUPER Youth Club Sir and Her Stunning Combo Analogue Black Dial Men and Women's Watch",
-      amount: "21,995.00",
-      qunatity: "2"
-    },
-    {
-      icon: "#shopping-bag",
-      title: "Fossil Q Gen 4 Hr Gold Dial Men's Smart Watch-FTW4017",
-      amount: "21,995.00",
-      qunatity: "1"
-    },
-    {
-      icon: "#shopping-bag",
-      title: "Macbook Pro 12-inch",
-      amount: "21,995.00",
-      qunatity: "1"
-    } */
-  ]
-
+  cartItems = []
+  saveforLater = []
   private _cartService: CartService
   constructor(
     cartService: CartService
@@ -48,27 +29,42 @@ export class CartComponent implements OnInit {
     .then((data: any) => {
       for(var i = 0; i < data.length; i++) {
         if(data[i].cartItem.in_cart) {
-          this.cartItems.push(
-            {
-              id: data[i].cartItem.product_id,
-              icon: this._imageUrl+data[i].itemDetails.images[0],
-              title: data[i].itemDetails.name,
-              amount: data[i].itemDetails.price,
-              qunatity: data[i].cartItem.quantity
-            } 
-          )
+          this.cartItems.push({
+            id: data[i].cartItem.product_id,
+            icon: this._imageUrl+data[i].itemDetails.images[0],
+            title: data[i].itemDetails.name,
+            amount: data[i].itemDetails.price,
+            qunatity: data[i].cartItem.quantity
+          })
         }
-        
+        else {
+          this.saveforLater.push({
+            id: data[i].cartItem.product_id,
+            icon: this._imageUrl+data[i].itemDetails.images[0],
+            title: data[i].itemDetails.name,
+            amount: data[i].itemDetails.price,
+            qunatity: data[i].cartItem.quantity
+          })
+        }
       }
-      console.log(data)
     })
   }
 
   deleteCartItem(id) {
     this._cartService.deleteCartItem(id)
     .subscribe(data => {
-      console.log(data)
       this.getCartItems()
     })
   } 
+
+  saveForLater(id, quantity) {
+    let cartItem: CartItem = new CartItem()
+    cartItem.product_id = id
+    cartItem.quantity = quantity
+    cartItem.in_cart = false
+    this._cartService.postCartItem(cartItem)
+    .subscribe(data => {
+      this.getCartItems()
+    })
+  }
 }
