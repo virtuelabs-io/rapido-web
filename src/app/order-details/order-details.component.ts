ec7628bc2572c1ca521bfe8013d100c2a9a0f27a
Anirup Patnaik <anirup049@gmail.com>
2019-07-21 03:38:44 +0530

    Basic Binding for Order summary

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../services/orders/orders.service';
 import { Order } from '../services/orders/order';
+import { Constants } from '../utils/constants';
 
 @Component({
   selector: 'app-order-details',
@@ -10,6 +11,7 @@ import { Order } from '../services/orders/order';
 export class OrderDetailsComponent implements OnInit {
   orderedItems = []
   deliveredAddress = []
+  _imageUrl: string = Constants.environment.staticAssets
   order: Order = new Order()
   private _orderService: OrdersService
 
@@ -30,7 +32,25 @@ export class OrderDetailsComponent implements OnInit {
     this._orderService.getOrder(this.order.order_id)
     .then((data: any) => {
       console.log(data)
-      this.orderedItems = data
+    //  this.orderedItems = data
+    for(var i = 0; i < data.length; i++) {
+      this.orderedItems.push(
+        {
+          pic: this._imageUrl+data[i].itemDetails.images[0],
+          title: data[i].itemDetails.name,
+          createdOn: data[i].orderItem.created_on,
+          productId: data[i].orderItem.product_id,
+          currency: data[i].itemDetails.currency,
+          orderId: data[i].orderItem.id,
+          order_price: data[i].orderItem.order_price,
+          order_status_id: data[i].orderItem.order_status_id,
+          quantity: data[i].orderItem.quantity,
+          unitPrice: data[i].orderItem.unit_price
+        }
+      )
+    }
+      
+      this.deliveredAddress = data[0].orderItem
     })
   }
 
