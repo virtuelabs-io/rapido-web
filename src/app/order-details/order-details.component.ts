8ad46dcd816fb24e46a939ddfd8495a2389cad2e
Anirup Patnaik <anirup049@gmail.com>
2019-07-21 05:57:25 +0530

    UI FIxes

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -11,6 +11,8 @@ import { Constants } from '../utils/constants';
 export class OrderDetailsComponent implements OnInit {
   orderedItems = []
   deliveredAddress = []
+  currency: string 
+  orderPrice: string
   _imageUrl: string = Constants.environment.staticAssets
   order: Order = new Order()
   private _orderService: OrdersService
@@ -32,13 +34,12 @@ export class OrderDetailsComponent implements OnInit {
     this._orderService.getOrder(this.order.order_id)
     .then((data: any) => {
       console.log(data)
-    //  this.orderedItems = data
     for(var i = 0; i < data.length; i++) {
       this.orderedItems.push(
         {
           pic: this._imageUrl+data[i].itemDetails.images[0],
           title: data[i].itemDetails.name,
-          createdOn: data[i].orderItem.created_on,
+          createdOn: data[i].orderItem.created_on.split("T")[0],
           productId: data[i].orderItem.product_id,
           currency: data[i].itemDetails.currency,
           orderId: data[i].orderItem.id,
@@ -49,8 +50,9 @@ export class OrderDetailsComponent implements OnInit {
         }
       )
     }
-      
-      this.deliveredAddress = data[0].orderItem
+    this.deliveredAddress = data[0].orderItem
+    this.currency = this.orderedItems[0].currency
+    this.orderPrice = data[0].orderItem.order_price
     })
   }
 
