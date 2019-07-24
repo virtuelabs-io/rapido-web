187e0fd7b61e6f2ca6ff5b713cdc37844de52ee6
sharan <sharan09@in.ibm.com>
2019-07-25 00:44:24 +0530

    WIP

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 import { SearchItemService } from '../shared-services/search-item/search-item.services';
 
 @Component({
@@ -12,24 +11,30 @@ export class ProductDetailsComponent implements OnInit {
 
   itemDetails: any
   itemId: any
+  mrpPrice: any
   constructor(private _searchItemService: SearchItemService,
-    private route: ActivatedRoute,
-    private router: Router ) { }
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.itemId = this.getItemId()
-
-    this._searchItemService.responsePoductListState.subscribe(respData => {
-      debugger
+    console.log(this.itemId)
+    if(this.itemId){
+      this._searchItemService.responsePoductListState.subscribe(respData => {
       let { hits } = respData
       if(hits && hits.hit ){
-        this.itemDetails = hits.hit[this.itemId].fields
+        this.itemDetails = hits.hit.filter((val,key) => {
+          return val.id == this.itemId
+        })
+        this.itemDetails = this.itemDetails[0].fields
+        this.mrpPrice = (this.itemDetails.price * (1 + parseFloat(this.itemDetails.offer))).toFixed(2)
+
       }
     })
+    }
+    
   }
 
   getItemId(){
-
     return this.route.params.getValue('id').id
   }
 
