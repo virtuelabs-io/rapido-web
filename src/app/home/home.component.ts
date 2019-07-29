318dff35f8b7e129b1f62e3395a642579e382018
Anirup Patnaik <anirup049@gmail.com>
2019-07-29 18:45:26 +0530

    Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,23 +13,23 @@ export class HomeComponent implements OnInit {
     this.cardDetails = [
       {
         "title": "Fashion photography store 1",
-        "image": "",
+        "image": '/assets/images/home_card_1.jpg',
         "desc": "find the best photography deals",
         "price": 100000
       },
       {
         "title": "Fashion photography store 2",
-        "image": "",
+        "image": '/assets/images/home_card_1.jpg',
         "desc": "find the best photography deals"
       },
       {
         "title": "Fashion photography store 3",
-        "image": "",
+        "image": '/assets/images/home_card_1.jpg',
         "desc": "find the best photography deals"
       },
       {
         "title": "Fashion photography store 4",
-        "image": "",
+        "image": '/assets/images/home_card_1.jpg',
         "desc": "find the best photography deals"
       }
     ]
