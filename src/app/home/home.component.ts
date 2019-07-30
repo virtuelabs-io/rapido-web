15820811e8c9004a2d6ad81158eea4d73335fdc9
Anirup Patnaik <anirup049@gmail.com>
2019-07-31 02:44:17 +0530

    UI Changes
    Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -37,34 +37,44 @@ export class HomeComponent implements OnInit {
 
     this.carousel = [
       {
-        "image": '/assets/images/home_card_1.jpg'
+        "image": '/assets/images/home_card_1.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_1.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_2.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_3.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_4.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_4.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_3.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
+      },
+      {
+        "image": '/assets/images/aboutUs_2.jpg',
+        "price": '₹ 359.00 to ₹ 5,319.00'
       },
       {
         "image": '/assets/images/aboutUs_1.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_2.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_3.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_4.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_4.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_3.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_2.jpg'
-      },
-      {
-        "image": '/assets/images/aboutUs_1.jpg'
+        
       },
       {
         "image": '/assets/images/home_card_1.jpg'
+        
       }
     ]
   }
