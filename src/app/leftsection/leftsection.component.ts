409dbd689aa080a6ec26be3f36c4b07507a57e1f
sharan <sharangowda2425@gmail.com>
2019-08-12 00:21:00 +0530

    TS pretify

--- src/app/leftsection/leftsection.component.ts
+++ src/app/leftsection/leftsection.component.ts
@@ -1,168 +1,187 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchItemService } from '../shared-services/search-item/search-item.services';
 
-@Component({
+ @Component({
   selector: 'app-leftsection',
   templateUrl: './leftsection.component.html',
   styleUrls: ['./leftsection.component.scss']
-})
-export class LeftSectionComponent implements OnInit {
-
-  @Input() closeDialog:any
-  filterData: Object 
-  prevQuery: Object 
-  selections: Object 
-  constructor(private _searchItemService: SearchItemService ) { }
+ })
+ export class LeftSectionComponent implements OnInit {
+ 
+  @Input() closeDialog: any
+  filterData: Object
+  prevQuery: Object
+  selections: Object
+  constructor(private _searchItemService: SearchItemService) {}
   fnPriceFilterHandler: Function;
   tags = []
   fieldsQuery: any
   searchedText: string = ""
-
+ 
   ngOnInit() {
-    this._searchItemService.responsePoductListState.subscribe(respData => {
-      this.updateProductControls(respData)
-    })
-    this._searchItemService.currentState.subscribe(query => {
-      if (query.searchedText){
-        this.searchedText = query.searchedText
-        this.prevQuery = query
-      }
-    })
-    this.fieldsQuery = {
-      price:{
-        q:null,
-        text:null
-      },
-      rating:{
-        q:null,
-        text:null
-      }
+   this.fieldsQuery = {
+    price: {
+     q: null,
+     text: null
+    },
+    rating: {
+     q: null,
+     text: null
+    }
+   }
+   this._searchItemService.responsePoductListState.subscribe(respData => {
+    this.updateProductControls(respData)
+  })
+  this._searchItemService.currentState.subscribe(query => {
+    if (query.searchedText) {
+      this.searchedText = query.searchedText
+      this.prevQuery = query
+    }
+  })
+  }
+  
+  updateProductControls(respData) {
+    let {
+      hits
+    } = respData
+    if (hits && hits.hit) {
+      this.tags = hits.hit[0].fields.tags
+      this.fnPriceFilterHandler = obj => this.priceFilterData(obj);
+      this.filterData = [{
+          'headerText': 'Filter by',
+          'expanded': 'true',
+          'panel': [{
+              'panelTitle': 'Rating',
+              'panelType': 'rating',
+              'panelData': ["4", "3", "2", "1"]
+            },
+            {
+              'panelTitle': 'Price',
+              'panelType': 'priceslider',
+              'panelData': {
+                'fnPriceFilterHandler': this.fnPriceFilterHandler,
+                'maxValue': 500, //dynamic
+                'minValue': 0 //dynamic
+              }
+            }
+          ]
+        },
+        {
+          'headerText': '',
+          'expanded': 'false',
+          'panel': [{
+            'panelTitle': 'Sort by',
+            'panelType': 'sort',
+            'panelData': [{
+                desc: 'Price: Low to High',
+                key: 'price asc'
+              },
+              {
+                desc: 'Price: High to Low',
+                key: 'price desc'
+              },
+              {
+                desc: 'Customer Review Low to High',
+                key: 'rating asc'
+              },
+              {
+                desc: 'Customer Review High to Low',
+                key: 'rating desc'
+              }
+            ]
+          }]
+        },
+        {
+          'headerText': 'Show related types',
+          'expanded': 'false',
+          'panel': [{
+            'panelTitle': this.searchedText || '',
+            'panelType': 'link',
+            'panelData': this.tags
+          }]
+        }
+      ]
     }
   }
-
-  updateProductControls(respData){
-    let {hits} = respData
-    if(hits && hits.hit ){
-    this.tags = hits.hit[0].fields.tags
-    this.fnPriceFilterHandler= obj => this.priceFilterData(obj);
-    this.filterData = [
-                      {
-                        'headerText':'Filter by',
-                        'expanded': 'true',
-                         'panel':[ 
-                           {
-                          'panelTitle':'Rating',
-                          'panelType':'rating',
-                          'panelData':["4","3","2","1"]
-                       },
-                        {
-                          'panelTitle':'Price',
-                          'panelType':'priceslider',
-                          'panelData':{
-                            'fnPriceFilterHandler':this.fnPriceFilterHandler,
-                            'maxValue':500,//dynamic
-                            'minValue':0//dynamic
-                          }
-                       },
-                       {
-                        'panelTitle':'Sort by',
-                        'panelType':'sort',
-                        'panelData':[
-                          {desc:'Price: Low to High', key:'price asc'},
-                          {desc:'Price: High to Low', key:'price desc'},
-                          {desc:'Avg. Customer Review', key:'rating asc'}
-                          // {desc:'Newest Arrivals', key:'date asc'}
-                        ]
-                      }
-                      ]},
-                      {
-                        'headerText':'Show related types',
-                        'expanded': 'false',
-                         'panel':[ {
-                           'panelTitle':this.searchedText || '',
-                           'panelType':'link',
-                           'panelData':this.tags
-                        }]
-                      },
-         ]
-        }
-  }
-
-  priceFilterData(range){
+  
+  priceFilterData(range) {
     let query = `(and '${this.searchedText}' (range field=price [${range.min}, ${range.max}]))`
-    if(this.fieldsQuery.rating.q){
-        query = `(and '${this.searchedText}' (and (range field=rating [${this.fieldsQuery.rating.q},${Number(5)}]) (range field=price [${range.min},${range.max}])))`
-      }
-      this.fieldsQuery.price.q = `[${range.min},${range.max}]`
-      this.fieldsQuery.price.text = ` $${range.min} - ${range.max}`
-      this.updateFilterConditions({
-        q:query,
-        searchedText:this.searchedText,
-        qdotparser:'structured',
-        parser:null
-      })
-  }
-
-  onPressRating(val){
-    let query = `(and '${this.searchedText}' (range field=rating [${val},${Number(5)}]))`
-    if(this.fieldsQuery.price.q){
-        query = `(and '${this.searchedText}' (and (range field=rating [${val},${Number(5)}]) (range field=price ${this.fieldsQuery.price.q})))`
-      }
-      this.fieldsQuery.rating.q = val
-      this.fieldsQuery.rating.text = ` ${val} - ${Number(5)}`
-      this.updateFilterConditions({
-        q:query,
-        searchedText:this.searchedText,
-        qdotparser:'structured',
-        parser:null
-      })
-  }
-
-  onPressSort(data){
+    if (this.fieldsQuery.rating.q) {
+      query = `(and '${this.searchedText}' (and (range field=rating [${this.fieldsQuery.rating.q},${Number(5)}]) (range field=price [${range.min},${range.max}])))`
+    }
+    this.fieldsQuery.price.q = `[${range.min},${range.max}]`
+    this.fieldsQuery.price.text = ` $${range.min} - ${range.max}`
     this.updateFilterConditions({
-      sort:data
+      q: query,
+      searchedText: this.searchedText,
+      qdotparser: 'structured',
+      parser: null
     })
   }
-
-  onPressItem(data){
+  
+  onPressRating(val) {
+    let query = `(and '${this.searchedText}' (range field=rating [${val},${Number(5)}]))`
+    if (this.fieldsQuery.price.q) {
+      query = `(and '${this.searchedText}' (and (range field=rating [${val},${Number(5)}]) (range field=price ${this.fieldsQuery.price.q})))`
+    }
+    this.fieldsQuery.rating.q = val
+    this.fieldsQuery.rating.text = ` ${val} - ${Number(5)}`
     this.updateFilterConditions({
-      q:data,
+      q: query,
+      searchedText: this.searchedText,
+      qdotparser: 'structured',
+      parser: null
+    })
+  }
+  
+  onPressSort(data) {
+    this.updateFilterConditions({
+      sort: data
+    })
+  }
+  
+  onPressItem(data) {
+    this.updateFilterConditions({
+      q: data,
       start: 0,
       sort: null,
       cursor: null,
       return: null,
-      qdotparser:null
+      qdotparser: null
     })
   }
-
-  updateFilterConditions(queryObj){
-    if(this.searchedText){
+  
+  updateFilterConditions(queryObj) {
+    if (this.searchedText) {
       this._searchItemService.changeState(queryObj)
-      if(this.closeDialog) {
+      if (this.closeDialog) {
         this.closeDialog.close()
       }
     }
   }
-
-  removeRating(){
-     this.fieldsQuery.rating.q = null
-     if(!this.fieldsQuery.price.q && !this.fieldsQuery.rating.q){
+  
+  removeRating() {
+    this.fieldsQuery.rating.q = null
+    if (!this.fieldsQuery.price.q && !this.fieldsQuery.rating.q) {
       this.onPressItem(this.searchedText)
-     }else{
-        let priceRange = JSON.parse(this.fieldsQuery.price.q) 
-        if(priceRange)
-        this.priceFilterData({min:priceRange[0], max:priceRange[1]})
-      }
+    } else {
+      let priceRange = JSON.parse(this.fieldsQuery.price.q)
+      if (priceRange)
+        this.priceFilterData({
+          min: priceRange[0],
+          max: priceRange[1]
+        })
+    }
   }
   
-  removePrice(){
+  removePrice() {
     this.fieldsQuery.price.q = null
-    if(!this.fieldsQuery.price.q && !this.fieldsQuery.rating.q){
+    if (!this.fieldsQuery.price.q && !this.fieldsQuery.rating.q) {
       this.onPressItem(this.searchedText)
-    }else{
+    } else {
       this.onPressRating(this.fieldsQuery.rating.q)
     }
-    
+  
   }
-
-}
+  
+  }
