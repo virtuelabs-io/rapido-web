449987008637745bfff98756445c77ce64309223
Anirup Patnaik <anirup049@gmail.com>
2019-08-12 18:12:18 +0530

    Add to cart feature in carousel component

--- src/app/card/carousel/carousel.component.ts
+++ src/app/card/carousel/carousel.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input, OnInit, SimpleChanges, Directive } from '@angular/core';
+import { CartItem } from '../../services/cart/cart-item';
+import { CartService } from '../../services/cart/cart.service';
+import { CartStateService } from '../../shared-services/cart-state/cart-state.service';
+import { LoginStateService } from '../../shared-services/login-state/login-state.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -12,7 +17,17 @@ export class CarouselComponent implements OnInit {
   @Input() carouselTitle = ""
   carouselData: any
   config: any
-  constructor() { }
+  isLoggedIn:Boolean
+  private _cartService: CartService
+  constructor(
+    private cartService: CartService,
+    private _cartStateService: CartStateService,
+    private _loginStateService: LoginStateService,
+    public router: Router
+  ) { 
+    this._cartService = cartService
+
+  }
 
   ngOnInit() {
     this.config = this.carouselConfig
@@ -47,4 +62,35 @@ export class CarouselComponent implements OnInit {
     }
     return formatedData
   }
+
+  async handleCart(id) {
+		
+		await this.loginSessinExists().
+		then( _ => this.postCartItem(id)).
+		then( _ => this._cartService.getCountOfInCartItems()).
+		then(count => this._cartStateService.updateCartCount(Number(count))).
+		catch(err => this.handleError(err))
+  }
+
+  async loginSessinExists(){
+    await (this._loginStateService.isLoggedInState.subscribe(state => this.isLoggedIn = state))
+ }
+  
+ async postCartItem(id) {
+    let cartItem: CartItem = new CartItem()
+		cartItem.product_id = parseInt(id) 
+    	cartItem.quantity = 1
+		cartItem.in_cart = true
+		if(this.isLoggedIn){
+			await this._cartService.postCartItem(cartItem).subscribe(data => data)
+		}else{
+			await Promise.reject("Login Session doesn't exist!")
+		}
+  }
+
+  async handleError(err){
+		if(err=="Login Session doesn't exist!"){
+			this.router.navigate(['/login', 'redirectToCart'])
+		}
+   }
 }
