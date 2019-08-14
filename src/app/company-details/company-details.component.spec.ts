1bf0aa83f3a0f4e47853792beaf5e97560a4df39
Anirup Patnaik <anirup049@gmail.com>
2019-08-15 04:05:22 +0530

    Test case file

--- src/app/company-details/company-details.component.spec.ts
+++ src/app/company-details/company-details.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { CompanyDetailsComponent } from './company-details.component';
-import { MatProgressSpinnerModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatFormFieldModule, MatProgressSpinnerModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CompanyDetailsComponent', () => {
   let component: CompanyDetailsComponent;
@@ -10,7 +12,7 @@ describe('CompanyDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, HttpClientModule, MatProgressSpinnerModule ],
+      imports: [ BrowserAnimationsModule, MatInputModule, HttpClientModule, RouterTestingModule, MatCardModule, MatFormFieldModule,ReactiveFormsModule, MatProgressSpinnerModule, FormsModule ],
       declarations: [ CompanyDetailsComponent ]
     })
     .compileComponents();
