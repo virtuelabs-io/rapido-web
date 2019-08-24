935ff7748396d95310f324a175ce1b0575657e51
sharan <sharangowda2425@gmail.com>
2019-08-24 20:01:32 +0530

    #114 Allowing oly positive integer

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart/cart.service';
 import { Constants } from '../../utils/constants';
+import { Common } from '../../utils/common';
 import { CartItem } from '../../services/cart/cart-item';
 import {MatSnackBar} from '@angular/material';
 import { Router } from '@angular/router';
@@ -53,10 +54,10 @@ export class CartComponent implements OnInit {
     await (this._loginStateService.isLoggedInState.subscribe(state => this.isLoggedIn = state))
   }
 
- async handleError(err) {
-  this.RouteService.changeRoute('cart')
-  this.router.navigateByUrl('/login')
- }
+  async handleError(err) {
+    this.RouteService.changeRoute('cart')
+    this.router.navigateByUrl('/login')
+  }
 
  async getCartItems() {
   this.cartItems = []
@@ -175,4 +176,8 @@ export class CartComponent implements OnInit {
     }
     this.cartAmount = this.cartAmount.toFixed(2)
   }
+
+  keyPress(event: any){
+		Common.allowPositiveNum(event)
+	}
 }
