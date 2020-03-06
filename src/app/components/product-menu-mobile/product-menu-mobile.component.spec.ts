f80d533100e8c8d233406b5c3f094b524c350a8c
Anirup Patnaik <anirup049@gmail.com>
2020-03-07 03:30:28 +0530

    Code refactoration

--- src/app/components/product-menu-mobile/product-menu-mobile.component.spec.ts
+++ src/app/components/product-menu-mobile/product-menu-mobile.component.spec.ts
@@ -1,15 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ProductMenuMobileComponent } from './product-menu-mobile.component';
 import { MatTreeModule, MatIconModule } from '@angular/material';
+import { Router, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProductMenuMobileComponent', () => {
   let component: ProductMenuMobileComponent;
   let fixture: ComponentFixture<ProductMenuMobileComponent>;
+  let router: Router;
+
+  // const routes: Routes = [
+  //   { path: 'products', component: LogInComponent}
+  // ]
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ MatTreeModule, MatIconModule ],
+      imports: [ MatTreeModule, MatIconModule, RouterTestingModule, HttpClientModule ],
       declarations: [ ProductMenuMobileComponent ]
     })
     .compileComponents();
