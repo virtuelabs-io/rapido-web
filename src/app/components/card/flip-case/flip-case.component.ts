0ade55d5cecdc59260af32879818631fcb18a7df
Anirup Patnaik <anirup049@gmail.com>
2020-03-12 01:00:41 +0530

    Restyling the new component

--- src/app/components/card/flip-case/flip-case.component.ts
+++ src/app/components/card/flip-case/flip-case.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-flip-case',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flip-case.component.scss']
 })
 export class FlipCaseComponent implements OnInit {
-
+  @Input()  flipImage = ""
   constructor() { }
 
   ngOnInit() {
