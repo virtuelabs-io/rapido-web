be38247d2481c5e424f784e3f02cdca0fea1162d
sharan <sharangowda2425@gmail.com>
2020-07-26 20:20:12 +0530

    Angular material
    Components can no longer be imported through "@angular/material". Use the individual secondary entry-points, such as @angular/material/button.

--- src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject, NgModule } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
